ffdbd60b01c91b4b4032ca9efe8ce9655712bf4e
wojciech.kniec <wojciech.kniec@5f9ad372-a0ce-43ef-bb14-72c95df1f956>
2016-08-31 13:39:40 +0000

    test
    git-svn-id: https://localhost:8443/svn/medusa@3 5f9ad372-a0ce-43ef-bb14-72c95df1f956

--- trunk/PythonMedusa/contour_finder.py
+++ trunk/PythonMedusa/contour_finder.py
@@ -2,16 +2,22 @@ import numpy as np
 
 from skimage import measure, io
 
+def find_contours(image):
+    r = img[:,:,0]
+    contours = measure.find_contours(r,0.5,  fully_connected='high', positive_orientation='high')
+    return contours
 
+#def filter_contours(contours, filter):
+#    return contours
 
 if __name__ == "__main__":
     img = io.imread('test.png')
-    r = img[:,:,0]
-    contours = measure.find_contours(r,0.5,  fully_connected='high', positive_orientation='high')
+    contours = find_contours(img)
     res = np.zeros(r.shape, dtype=np.int)
-
     for n, contour in enumerate(contours):
         for x,y in contour:
             res[int(x+0.5),int(y+0.5)] = 255
 
     io.imsave('test.res.png', res)
+
+
