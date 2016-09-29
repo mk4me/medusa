855fadd1b1c891b230a5222d8a08e4dbe818dfa7
motion <motion@5f9ad372-a0ce-43ef-bb14-72c95df1f956>
2016-09-29 14:13:47 +0000

    gray2rgb update
    git-svn-id: https://localhost:8443/svn/medusa@13 5f9ad372-a0ce-43ef-bb14-72c95df1f956

--- trunk/MatlabMedusa/AP/pjConverter.m
+++ trunk/MatlabMedusa/AP/pjConverter.m
@@ -3,7 +3,7 @@ function pjConverter(input, output)
     if not(isdir(output))
         mkdir(output)
     end
-    
+    % '../data/20160916-cleanIL_picture_set/all/','../data/out-AP/'
     files = dir(strcat(input, '/*.png'));
     fileID = fopen(strcat(output, '/data-popowicz.csv'),'w');
     %inputImage = '2013-10-15-S0012-T0022.png';
@@ -15,7 +15,7 @@ function pjConverter(input, output)
         if (complete)
             outFile = strcat(output, './', name, '-bin-popowicz.png');
 			grayImage = im2bw(synovatisImage, 0.2);
-			rgbImage = cat(3, grayImage, grayImage, grayImage);
+			rgbImage = uint8(cat(3, grayImage.*255, grayImage.*255, grayImage.*255));
             imwrite(rgbImage, outFile);
             annotationsCount = csvCreator.getNumberOfBones(boneData) + 2;
             csvCreator.writeFileHeader(fileID, name, annotationsCount);
